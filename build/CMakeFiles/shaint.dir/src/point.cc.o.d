b010ce8a0b4cd27343f80ff0d928a6809daaad2f
savipauk <erik.kranjec8@outlook.com>
2025-06-15 23:01:28 +0200

    add gitignore

--- build/CMakeFiles/shaint.dir/src/point.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/shaint.dir/src/point.cc.o: \
- /home/erik/design-patterns/shaint/src/point.cc \
- /usr/include/stdc-predef.h /home/erik/design-patterns/shaint/src/point.h
--- build/CMakeFiles/shaint.dir/src/rectangle.cc.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/shaint.dir/src/rectangle.cc.o: \
- /home/erik/design-patterns/shaint/src/rectangle.cc \
- /usr/include/stdc-predef.h \
- /home/erik/design-patterns/shaint/src/rectangle.h
